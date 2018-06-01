51fabd843528972f01f682d262a4baa8dff31d53
lindongwu <lindongwu@memect.co>
2018-06-01 15:16:32 +0800

    feat: style for list

--- src/pages/Blocks.js
+++ src/pages/Blocks.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Flex from 'styled-flex-component';
-import { Spin, Table, Breadcrumb } from 'antd';
+import { Spin, Table } from 'antd';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { translate } from 'react-i18next';
@@ -10,11 +10,28 @@ import { Link } from 'react-router-dom';
 
 import type { BlockData } from '../store/block';
 
-const Container = styled(Flex)``;
+const Container = styled(Flex)`
+  .ant-spin-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .ant-table {
+    width: 100%;
+  }
+  .ant-table-pagination.ant-pagination {
+    float: unset;
+  }
+
+  li.ant-pagination-jump-next + li.ant-pagination-item {
+    display: none;
+  }
+`;
 
 type Props = {};
 type Store = {
   list: BlockData[],
+  loading: boolean,
 };
 type Dispatch = {
   getBlocksList: () => void,
@@ -27,44 +44,43 @@ class Blocks extends Component<Props & Store & Dispatch, *> {
   }
   render() {
     return (
-      <Container column>
-        {/* <pre>
-          <code>{JSON.stringify(this.props.list, null, '  ')}</code>
-        </pre> */}
-        <Table dataSource={this.props.list}>
-          <Table.Column
-            title={this.props.t('blockNum')}
-            dataIndex="blockNum"
-            key="blockNum"
-            render={blockNum => <Link to={`/block/${blockNum}`}>{blockNum}</Link>}
-          />
-          <Table.Column
-            title={this.props.t('createdAt')}
-            dataIndex="createdAt"
-            key="createdAt"
-            render={({ sec }) => sec}
-          />
-          <Table.Column
-            title={this.props.t('transactions')}
-            dataIndex="transactions"
-            key="transactions"
-            render={JSON.stringify(transactions =>
-              transactions.map(({ $id }) => <Link to={`/transaction/${$id}`}>{$id}</Link>),
-            )}
-          />
-          <Table.Column
-            title={this.props.t('producerAccountId')}
-            dataIndex="producerAccountId"
-            key="producerAccountId"
-            render={producerAccountId => <Link to={`/account/${producerAccountId}`}>{producerAccountId}</Link>}
-          />
-        </Table>
-      </Container>
+      <Spin tip="Connecting" spinning={this.props.loading} size="large">
+        <Container column>
+          <Table dataSource={this.props.list}>
+            <Table.Column
+              title={this.props.t('blockNum')}
+              dataIndex="blockNum"
+              key="blockNum"
+              render={blockNum => <Link to={`/block/${blockNum}`}>{blockNum}</Link>}
+            />
+            <Table.Column
+              title={this.props.t('createdAt')}
+              dataIndex="createdAt"
+              key="createdAt"
+              render={({ sec }) => sec}
+            />
+            <Table.Column
+              title={this.props.t('transactions')}
+              dataIndex="transactions"
+              key="transactions"
+              render={JSON.stringify(transactions =>
+                transactions.map(({ $id }) => <Link to={`/transaction/${$id}`}>{$id}</Link>),
+              )}
+            />
+            <Table.Column
+              title={this.props.t('producerAccountId')}
+              dataIndex="producerAccountId"
+              key="producerAccountId"
+              render={producerAccountId => <Link to={`/account/${producerAccountId}`}>{producerAccountId}</Link>}
+            />
+          </Table>
+        </Container>
+      </Spin>
     );
   }
 }
 
-const mapState = ({ block: { list } }): Store => ({ list });
+const mapState = ({ block: { list }, info: { loading } }): Store => ({ list, loading });
 const mapDispatch = ({ block: { getBlocksList } }): Dispatch => ({ getBlocksList });
 export default withRouter(
   translate()(
--- src/store/utils.js
+++ src/store/utils.js
@@ -1,6 +1,7 @@
 // for table
+const navHeight = 64;
 const rowHeight = 54;
 const paginationHeight = 48;
 export const getDisplayAreaHeight = () =>
   document.documentElement ? document.documentElement.clientHeight : window.innerHeight;
-export const getPageSize = () => Math.floor((getDisplayAreaHeight() - paginationHeight) / rowHeight) - 1;
+export const getPageSize = () => Math.floor((getDisplayAreaHeight() - navHeight - paginationHeight) / rowHeight) - 1;
