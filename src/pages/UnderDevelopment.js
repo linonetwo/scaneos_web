187ecce3d63bac80299cfbf023bb86efb49672be
lindongwu <lindongwu@memect.co>
2018-06-02 17:04:55 +0800

    chore: remove useless code

--- src/pages/UnderDevelopment.js
+++ src/pages/UnderDevelopment.js
@@ -2,8 +2,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Flex from 'styled-flex-component';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 const Container = styled(Flex)`
   overflow-x: hidden;
@@ -13,20 +11,13 @@ const Content = styled(Flex)`
   width: 100%;
 `;
 
-type Props = {
-  match: {
-    params: {
-      blockId: string,
-    },
-  },
-};
+type Props = {};
 type Store = {};
 type Dispatch = {};
 
-class UnderDevelopment extends Component<Props & Store & Dispatch, *> {
+export default class UnderDevelopment extends Component<Props & Store & Dispatch, *> {
   state = {};
   render() {
-    const currentTransactionID = this.props.currentTransactionID || this.props.match.params.transactionId;
     return (
       <Container>
         <Content center>Under Development...</Content>
@@ -34,12 +25,3 @@ class UnderDevelopment extends Component<Props & Store & Dispatch, *> {
     );
   }
 }
-
-const mapState = ({}): Store => ({});
-const mapDispatch = ({}): Dispatch => ({});
-export default withRouter(
-  connect(
-    mapState,
-    mapDispatch,
-  )(UnderDevelopment),
-);
