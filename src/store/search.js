68a3aa665444353b1b37bdd3358cb6d69ccf5642
lindongwu <lindongwu@memect.co>
2018-06-02 16:43:05 +0800

    feat: search blockId

--- src/store/search.js
+++ src/store/search.js
@@ -1,19 +1,14 @@
 // @flow
-import { initial } from 'lodash';
-import camelize from 'camelize';
+import get from '../API.config';
 
 type Store = {
+  loading: boolean,
   keyWord: string,
-  results: Object[],
-  pagination: { currentTotal: number, loadable: boolean, pageCountToLoad: number },
-  currentPage: number,
 };
 
 const defaultState = {
+  loading: false,
   keyWord: '',
-  results: [],
-  pagination: { currentTotal: 0, loadable: false, pageCountToLoad: 10 },
-  currentPage: 0,
 };
 export default (initialState?: Object = {}) => ({
   state: {
@@ -21,87 +16,46 @@ export default (initialState?: Object = {}) => ({
     ...initialState,
   },
   reducers: {
+    toggleLoading(state: Store) {
+      state.loading = !state.loading;
+      return state;
+    },
     changeKeyWord(state: Store, keyWord: string) {
       state.keyWord = keyWord;
       return state;
     },
-    setPage(state: Store, newPage: number) {
-      state.currentPage = newPage;
-      return state;
-    },
-    clearState(state: Store) {
-      state = defaultState;
-      return state;
-    },
-    initResult(state: Store, results: SearchResult[]) {
-      state.results = results;
-      state.pagination = { currentTotal: 0, loadable: false, pageCountToLoad: state.pagination.pageCountToLoad };
-      return state;
-    },
-    appendResult(state: Store, results: SearchResult[]) {
-      state.results = [...state.results, ...results];
-      return state;
-    },
-    increaseOffset(state: Store, newOffset: number, loadable: boolean) {
-      state.pagination.currentTotal += newOffset;
-      state.pagination.loadable = loadable;
-      return state;
-    },
   },
   effects: {
-    async search(gotoPage?: number) {
+    async search() {
       const {
-        store: { dispatch, filter, search },
+        store: { dispatch, getState },
       } = await import('./');
       dispatch.info.toggleLoading();
-      dispatch.queryString.updateURI();
-
-      if (!gotoPage) {
-        this.clearState();
-        this.setPage(0);
-      }
-
-      const { getPageSize } = await import('./utils');
-      const pageSize = getPageSize();
-      const body = JSON.stringify({
-        keywords: filter.keyWord.split(/\s+/g),
-
-        offset: gotoPage ? search.pagination.currentTotal : 0,
-        limit: pageSize * search.pagination.pageCountToLoad + 1,
-      });
+      dispatch.message.toggleLoading();
+      const {
+        search: { keyWord },
+      } = await getState();
 
       try {
-        let results: SearchResult[] = await fetch(`${API}/search`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body,
-        })
-          .then(res => res.json())
-          .then(camelize);
+        const data = await get(`/search?query=${keyWord}`);
+        const { history } = await import('./history');
 
-        const loadable = results.length === pageSize * search.pagination.pageCountToLoad + 1;
-
-        if (loadable) {
-          results = initial(results);
+        if (data.blockId === keyWord && typeof data.blockNum === 'number') {
+          history.push(`/block/${data.blockNum}`);
         }
-        if (gotoPage) {
-          dispatch.search.appendResult(results);
-        } else {
-          dispatch.search.initResult(results);
-        }
-        this.increaseOffset(results.length, loadable);
       } catch (error) {
         console.error(error);
         const errorString = error.toString();
         let notificationString = errorString;
         if (errorString.match(/^SyntaxError: Unexpected token/)) {
-          notificationString = '与后端失去有效连接，可能网络不畅，或服务器出现故障（请联系工程师）';
+          notificationString = 'Connection lost, maybe due to some Network error.';
         } else if (errorString.match(/^TypeError/)) {
-          notificationString = '网络连接有问题，请求数据失败';
+          notificationString = 'Failed to fetch data from server.';
         }
         dispatch.info.displayNotification(notificationString);
       } finally {
         dispatch.info.toggleLoading();
+        dispatch.message.toggleLoading();
       }
     },
   },
