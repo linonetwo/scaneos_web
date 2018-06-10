bba2c27226f77d0766517cfa1ed416dfdc85219c
lindongwu <lindongwu@memect.co>
2018-06-10 13:52:13 +0800

    feat: test for block detail

--- src/pages/Block/index.fixture.js
+++ src/pages/Block/index.fixture.js
@@ -1,9 +1,23 @@
 import Component from './';
 
+import { API } from '../../API.config';
+import { blockData1 } from '../../store/block.data';
+
 export default [
   {
     component: Component,
-    name: '概览',
+    name: '区块详情',
     props: {},
+    url: `/block/${blockData1.blockNum}`,
+    route: '/block/:blockNum',
+    fetch: [
+      {
+        matcher: () => `${API}/blocks?block_id=${blockData1.blockId}`,
+        response: JSON.stringify(blockData1),
+      },
+    ],
+    reduxState: {
+      block: {},
+    },
   },
 ];
