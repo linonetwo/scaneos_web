8b960ef6086c1ee9d78f4c376ba7b492b62e9554
lindongwu <lindongwu@memect.co>
2018-06-10 14:31:53 +0800

    feat: test for Oerview

--- src/components/OverviewList/index.fixture.js
+++ src/components/OverviewList/index.fixture.js
@@ -1,9 +1,51 @@
 import Component from './';
 
+import { API } from '../../API.config';
+import { defaultState } from '../../store/block';
+import { getPageSize } from '../../store/utils';
+
+const getDefaultPageSize = () => getPageSize() * defaultState.pagination.pageCountToLoad + 1;
+
 export default [
   {
     component: Component,
     name: '概览',
     props: {},
+    url: '/',
+    route: '/',
+    fetch: [
+      {
+        matcher: () => `${API}/stats`,
+        response: () =>
+          import('../../store/aggregation.data')
+            .then(({ statsData1 }) => statsData1)
+            .then(res => JSON.stringify(res)),
+      },
+      {
+        matcher: () => `${API}/block?page=0&size=${getDefaultPageSize()}`,
+        response: () =>
+          import('../../store/block.data')
+            .then(({ listData1 }) => listData1)
+            .then(res => JSON.stringify(res)),
+      },
+      {
+        matcher: () => `${API}/transactions?page=0&size=${getDefaultPageSize()}`,
+        response: () =>
+          import('../../store/transaction.data')
+            .then(({ listData1 }) => listData1)
+            .then(res => JSON.stringify(res)),
+      },
+      {
+        matcher: () => `${API}/accounts?page=0&size=${getDefaultPageSize()}`,
+        response: () => [],
+      },
+      {
+        matcher: () => `${API}/actions?page=0&size=${getDefaultPageSize()}`,
+        response: () => [],
+      },
+    ],
+    reduxState: {
+      transaction: {},
+    },
   },
 ];
