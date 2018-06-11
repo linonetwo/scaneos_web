1c89c80b12c0157cc411067675b5ee2c2f287029
lindongwu <lindongwu@memect.co>
2018-06-11 15:33:14 +0800

    Revert "chore: use new API"
    This reverts commit b63ccd082da0c5648ce34c48a886d065cc01a170.

--- src/API.config.js
+++ src/API.config.js
@@ -1,7 +1,7 @@
 // @flow
 import camelize from 'camelize';
 
-export const API = 'http://scaneos1.io/api';
+export const API = 'http://scaneos.io:3128';
 export const EOS_API = 'http://138.68.31.191:8888';
 export default (path: string) =>
   fetch(`${API}${path}`)
