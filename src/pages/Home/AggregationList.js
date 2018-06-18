02759343e0237e52eee5084a48eeffbda97e4a7e
lindongwu <lindongwu@memect.co>
2018-06-18 22:58:47 +0800

    feat: polling aggragation data

--- src/pages/Home/AggregationList.js
+++ src/pages/Home/AggregationList.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import Flex from 'styled-flex-component';
 import { Spin } from 'antd';
@@ -68,9 +68,9 @@ const PriceChangeContainer = styled(Flex)`
   margin-left: 5px;
 `;
 
-
 type Props = {
   t: Function,
+  getAggregationData: () => void,
 };
 type Store = {
   loading: boolean,
@@ -79,74 +79,85 @@ type Store = {
   currentPriceData: CurrentPriceData,
 };
 type Dispatch = {
-  getAggregationData: () => void,
   getCurrentPriceData: () => void,
 };
-function AggregationList(props: Props & Store) {
-  const priceUp = props.currentPriceData.percentChange24h > 0;
-  const { t } = props;
-  return (
-    <AggregationContainer wrap="true">
-      <Spin spinning={props.loading}>
-        <Link to="/blocks/">
-          <AggregationItem column center>
-            <h4>{t('blocksNum')}</h4>
-            {props.data.blockNumber}
-          </AggregationItem>
-        </Link>
-      </Spin>
-      <Spin spinning={props.loading}>
-        <Link to="/transactions/">
-          <AggregationItem column center>
-            <h4>{t('transactionNum')}</h4>
-            {props.data.transactionNumber}
-          </AggregationItem>
-        </Link>
-      </Spin>
-      <Spin spinning={props.priceLoading}>
-        <Link to="/price/">
-          <AggregationItem column center>
-            <h4>
-              {t('price')}
-              <PriceChangeContainer up={priceUp} center>
-                {priceUp ? '+' : ''}
-                {props.currentPriceData.percentChange24h}%
-              </PriceChangeContainer>
-            </h4>
-            {numeral(props.currentPriceData.priceUsd)
-              .format('($0.00 a)')
-              .replace('b', 'B')}
-          </AggregationItem>
-        </Link>
-      </Spin>
-      <Spin spinning={props.priceLoading}>
-        <Link to="/price/">
-          <AggregationItem column center>
-            <h4>{t('marketCap')}</h4>
-            {numeral(props.currentPriceData.marketCapUsd)
-              .format('($0.00 a)')
-              .replace('b', 'B')}
-          </AggregationItem>
-        </Link>
-      </Spin>
-      <Spin spinning={props.loading}>
-        <Link to="/accounts/">
-          <AggregationItem column center>
-            <h4>{t('accountNum')}</h4>
-            {props.data.accountNumber}
-          </AggregationItem>
-        </Link>
-      </Spin>
-      <Spin spinning={props.loading}>
-        <Link to="/actions/">
-          <AggregationItem column center>
-            <h4>{t('actionNum')}</h4>
-            {props.data.actionNumber}
-          </AggregationItem>
-        </Link>
-      </Spin>
-    </AggregationContainer>
-  );
+class AggregationList extends PureComponent<Props & Store> {
+  componentDidMount() {
+    this.polling = setInterval(() => {
+      this.props.getAggregationData();
+    }, 2000);
+  }
+  componentWillUnmount() {
+    this.polling && clearInterval(this.polling);
+  }
+  polling = null;
+
+  render() {
+    const priceUp = this.props.currentPriceData.percentChange24h > 0;
+    const { t } = this.props;
+    return (
+      <AggregationContainer wrap="true">
+        <Spin spinning={this.props.loading}>
+          <Link to="/blocks/">
+            <AggregationItem column center>
+              <h4>{t('blocksNum')}</h4>
+              {this.props.data.blockNumber}
+            </AggregationItem>
+          </Link>
+        </Spin>
+        <Spin spinning={this.props.loading}>
+          <Link to="/transactions/">
+            <AggregationItem column center>
+              <h4>{t('transactionNum')}</h4>
+              {this.props.data.transactionNumber}
+            </AggregationItem>
+          </Link>
+        </Spin>
+        <Spin spinning={this.props.priceLoading}>
+          <Link to="/price/">
+            <AggregationItem column center>
+              <h4>
+                {t('price')}
+                <PriceChangeContainer up={priceUp} center>
+                  {priceUp ? '+' : ''}
+                  {this.props.currentPriceData.percentChange24h}%
+                </PriceChangeContainer>
+              </h4>
+              {numeral(this.props.currentPriceData.priceUsd)
+                .format('($0.00 a)')
+                .replace('b', 'B')}
+            </AggregationItem>
+          </Link>
+        </Spin>
+        <Spin spinning={this.props.priceLoading}>
+          <Link to="/price/">
+            <AggregationItem column center>
+              <h4>{t('marketCap')}</h4>
+              {numeral(this.props.currentPriceData.marketCapUsd)
+                .format('($0.00 a)')
+                .replace('b', 'B')}
+            </AggregationItem>
+          </Link>
+        </Spin>
+        <Spin spinning={this.props.loading}>
+          <Link to="/accounts/">
+            <AggregationItem column center>
+              <h4>{t('accountNum')}</h4>
+              {this.props.data.accountNumber}
+            </AggregationItem>
+          </Link>
+        </Spin>
+        <Spin spinning={this.props.loading}>
+          <Link to="/actions/">
+            <AggregationItem column center>
+              <h4>{t('actionNum')}</h4>
+              {this.props.data.actionNumber}
+            </AggregationItem>
+          </Link>
+        </Spin>
+      </AggregationContainer>
+    );
+  }
 }
 
 const mapState = ({ aggregation: { loading, data }, price: { loading: priceLoading, currentPriceData } }): Store => ({
