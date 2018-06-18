941fedf26d8c3b329d7e490c33454e81d744a9ca
lindongwu <lindongwu@memect.co>
2018-06-18 12:22:14 +0800

    bugfix: search no loading indicator

--- src/store/search.js
+++ src/store/search.js
@@ -28,30 +28,12 @@ export default (initialState?: Object = {}) => ({
   effects: {
     async searchKeyWord(param: { keyWord: string, type: string }) {
       const { keyWord, type } = param;
-      const {
-        store: { dispatch },
-      } = await import('./');
-      dispatch.info.toggleLoading();
-      try {
-        if (type === 'account') {
-          const data = await get(`/search?type=${type}&name=${keyWord}`);
-          return data;
-        }
-        const data = await get(`/search?type=${type}&id=${keyWord}`);
+      if (type === 'account') {
+        const data = await get(`/search?type=${type}&name=${keyWord}`);
         return data;
-      } catch (error) {
-        console.error(error);
-        const errorString = error.toString();
-        let notificationString = errorString;
-        if (errorString.match(/^SyntaxError: Unexpected token/)) {
-          notificationString = 'Connection lost, maybe due to some Network error.';
-        } else if (errorString.match(/^TypeError/)) {
-          notificationString = 'Failed to fetch data from server.';
-        }
-        dispatch.info.displayNotification(notificationString);
-      } finally {
-        dispatch.info.toggleLoading();
       }
+      const data = await get(`/search?type=${type}&id=${keyWord}`);
+      return data;
     },
     async search() {
       const {
@@ -69,20 +51,25 @@ export default (initialState?: Object = {}) => ({
       // 搜索黏贴的时候可能带上了空格
       keyWord = keyWord.replace(/\s/g, '');
       if (keyWord.replace(/\s/g, '').length === 64) {
+        dispatch.block.toggleLoading();
+        dispatch.transaction.toggleLoading();
         // 长度为 64 的可能是 blockId，或者 transactionId
         const data = await this.searchKeyWord({ keyWord, type: 'block' }).then(res =>
           dispatch.block.getFirstBlockIdFromBlockListResponse(res),
         );
+        dispatch.block.toggleLoading();
+        dispatch.transaction.toggleLoading();
         if (data && data.blockId === keyWord && typeof data.blockNum === 'number') {
-          dispatch.block.getBlockData(data.blockNum);
           return history.push(`/block/${data.blockNum}`);
         }
         return history.push(`/transaction/${keyWord}`);
       } else if (keyWord.length === 53) {
         // 还可能是账号公钥
+        dispatch.account.toggleLoading();
         const data: { accountNames: string[] } = await postEOS('/history/get_key_accounts', { key: keyWord });
         if (data.accountNames.length > 0) {
           // 目前先取这个公钥下的第一个账号，如果真的会有多个账号我们可以返回列表？
+          dispatch.account.toggleLoading();
           return history.push(`/account/${data.accountNames[0]}`);
         }
       }
