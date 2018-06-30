5301ab5268b8c38368d1b60307b028733a106821
linonetwo <linonetwo012@gmail.com>
2018-06-30 17:09:22 +0800

    feat: GraphQL Store rehydration

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,6 +1,10 @@
+/* eslint-env browser */
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import { GRAPHQL_API } from '../API.config';
 
 export default new ApolloClient({
   uri: GRAPHQL_API,
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
 });
